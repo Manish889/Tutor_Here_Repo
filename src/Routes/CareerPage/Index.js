fc5205c71739b2bde14a7abddfde6852a41ae445
Pawandeep <78660135+pawandeep98@users.noreply.github.com>
2021-08-14 20:43:24 +0530

    Update Index.js

--- src/Routes/CareerPage/Index.js
+++ src/Routes/CareerPage/Index.js
@@ -4,7 +4,7 @@ import Career from './../../MyComponents/Career/Index';
 import JobTable from './../../MyComponents/JobTable/Index';
 import SuggestionBox from './../../MyComponents/SuggestionBox/Index';
 import Footer from './../../MyComponents/Footer/Index';
-import ChatBox from '../../MyComponents/ChatBox/Index';
+
 import Circles from '../../MyComponents/Circles/circles';
 import Cardview from '../../MyComponents/Title/Cardview';
 import Calendar from '../../MyComponents/calendar/Calendar';
@@ -14,7 +14,7 @@ export default function Index() {
     return (
         <>
             <Header />
-            {/* <ChatBox/> */}
+            
             <Career />
             <Cardview/>
             <Circles/>

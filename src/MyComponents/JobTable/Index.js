c513d1a90bd2b62778cc2167c2f1df65ea2358f7
Satnam-Singh-1 <ssingh9@ce.iitr.ac.in>
2021-08-22 20:33:05 +0530

    homepage final push 1

--- src/MyComponents/JobTable/Index.js
+++ src/MyComponents/JobTable/Index.js
@@ -3,14 +3,14 @@ import "./style.css"
 
 export default function Index() {
     return (
-        <section className="cardviewjob">
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="col">
-                    <div class="card h-100">
+        <section className="job-cardview">
+            <div class="job-row">
+                <div class="job-col">
+                    <div class="job-card">
                         <img src="https://tutorhere.in/wp-content/uploads/2021/07/lagos-techie-IgUR1iX0mqM-unsplash-1-1024x683.jpg" class="card-img-top"/>
                         <div class="card-body">
-                            <h5 class="card-title">Relay Learning – The Future of Learning</h5>
-                            <p class="card-text">Photo by Lagos Techie on Unsplash Relay learning is a concept that is similar to the activity, relay, itself. Confused? Okay, first. How does a relay race work? We have</p>
+                            <h5 class="job-card-title">Relay Learning – The Future of Learning</h5>
+                            <p class="job-\\card-text">Photo by Lagos Techie on Unsplash Relay learning is a concept that is similar to the activity, relay, itself. Confused? Okay, first. How does a relay race work? We have</p>
                             <a href="#">Read More</a>
                         </div>
                         <div class="card-footer">
@@ -18,12 +18,12 @@ export default function Index() {
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
+                <div class="job-col">
+                    <div class="job-card">
                         <img src="https://tutorhere.in/wp-content/uploads/2021/07/giammarco-zeH-ljawHtg-unsplash-1024x683.jpg" class="card-img-top"/>
                         <div class="card-body">
-                            <h5 class="card-title">3 Ancient Philosophies That Benefit Contemporary Students</h5>
-                            <p class="card-text">Photo by Giammarco on Unsplash Ancient Philosophy has mostly been considered as boorish and antiquated by the younger minds. Pfft! The whole idea of reading up on ancient philosophers is</p>
+                            <h5 class="job-card-title">3 Ancient Philosophies That Benefit Contemporary Students</h5>
+                            <p class="job-\\card-text">Photo by Giammarco on Unsplash Ancient Philosophy has mostly been considered as boorish and antiquated by the younger minds. Pfft! The whole idea of reading up on ancient philosophers is</p>
                             <a href="#">Read More</a>
                         </div>
                         <div class="card-footer">
@@ -31,12 +31,12 @@ export default function Index() {
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
+                <div class="job-col">
+                    <div class="job-card">
                         <img src="https://tutorhere.in/wp-content/uploads/2021/07/pod-fe-1024x682.jpeg" class="card-img-top"/>
                         <div class="card-body">
-                            <h5 class="card-title">Podcasts for Effective Learning: 5 Reasons to Adopt Podcasting</h5>
-                            <p class="card-text">Photo by Anna Tarazevich from Pexels Podcasting, as one of the newest media in the mainstream, and one of the forefront technologies. Podcasts have a positive influence on effective learning. Thanks to the</p>
+                            <h5 class="job-card-title">Podcasts for Effective Learning: 5 Reasons to Adopt Podcasting</h5>
+                            <p class="job-\\card-text">Photo by Anna Tarazevich from Pexels Podcasting, as one of the newest media in the mainstream, and one of the forefront technologies. Podcasts have a positive influence on effective learning. Thanks to the</p>
                             <a href="#">Read More</a>
                         </div>
                         <div class="card-footer">

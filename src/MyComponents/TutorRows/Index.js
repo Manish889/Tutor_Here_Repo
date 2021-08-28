9f9647aed1ba25e14a5c9e3e7b40f7fbf174f80c
im-somax <patel.som10@gmail.com>
2021-08-28 20:27:00 +0530

    28 august

---
+++ src/MyComponents/TutorRows/Index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const TutorRows = () => {
+    return (
+        <>
+            <div class="media">
+                <img class="mr-3" src="..." />
+                    <div class="media-body">
+                        <h5 class="mt-0">Media heading</h5>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+            </div>
+        </>
+    );
+};
+
+export default TutorRows;
--- src/Routes/Tutors/Index.js
+++ src/Routes/Tutors/Index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../MyComponents/Header/Navbar';
 import Footer from '../../MyComponents/Footer/Index';
+import TutorRows from '../../MyComponents/TutorRows/Index';
 import BgImgText from '../../MyComponents/BgImgText/BgImgText';
  const LearnersPage = () => {
      return (
         <>
         <Header/>
         <BgImgText toptext='Tutors' bottomtext='A tutors job is to take a bunch of live wires and see that they are well-grounded.' />
+        <TutorRows/>
         <Footer />
         </>
      );

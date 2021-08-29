f4f81304e6f536db46ff81f8564793cd4f01b6fd
Satnam-Singh-1 <ssingh9@ce.iitr.ac.in>
2021-08-29 16:47:30 +0530

    Tutors update 29 august

--- src/MyComponents/TutorRows/Index.js
+++ src/MyComponents/TutorRows/Index.js
@@ -1,20 +1,18 @@
-import {DataTutors} from "../../Data/Data";
+import { DataTutors } from "../../Data/Data";
 import React from "react";
 import "./style.css";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const TutorRows = () => {
-    return (
-        <>
-            {DataTutors.map((n) => {
-              return (
-                <div class="profile md-3">
-                    <img src={n.image} alt ="" className="profile__image"/>
-                    <div class="profile__name">Name : {n.name} </div>
-                    <div class="profile__designation">Designaion : {n.designation}</div>
-                    <div class="profile__fulldesignation"> {n.fullDesignation}</div> 
-                    <a className="profile__linkedIn" href={n.linkedIn} target="_blank">
-                        <LinkedInIcon
+  return (
+    <>
+      {DataTutors.map((n) => {
+        return (
+          <div class="profile md-3 mt-3 col-8 ">
+            <a className="profile__image" href={n.linkedIn} target="_blank">
+              <img src={n.image} alt="" className="profile__ico" />
+              <div class="middle">
+              <LinkedInIcon
                             style={{
                                 color: "blue",
                                 fontSize: "2.5rem",
@@ -22,12 +20,23 @@ const TutorRows = () => {
                                 borderRadius: ".5rem",
                             }}
                         />
-                    </a>
-                </div>
-              );
-            })}
-        </>
-    );
+              </div>
+            </a>
+            <div className="tutors-detail">
+              <div class="profile__name"> {n.name} </div>
+              <div class="profile__designation">
+                Designaion : {n.designation}
+              </div>
+              <div class="profile__fulldesignation"> {n.fullDesignation}</div>
+
+              
+            </div>
+          </div>
+        );
+      })}
+      ;
+    </>
+  );
 };
 
 export default TutorRows;
